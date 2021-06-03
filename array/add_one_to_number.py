287535b45e1d98b173c44e51263f2d529effe601
kushal1999seemakurthi <47604342+kushal1999seemakurthi@users.noreply.github.com>
2021-06-03 08:31:05 +0530

    Delete array directory

--- array/add_one_to_number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    # @param A : list of integers
-    # @return a list of integers
-    def plusOne(self, A):
-        p=map(str,A)
-        p="".join(p)
-        p=int(p)+1
-        return p
